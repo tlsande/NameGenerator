4f2bc1f61e530b21492d38b4d477fd2f54a4a2ff
tlsande <tlsande@g.clemson.edu>
2018-06-06 19:09:30 -0400

    Basic dumb generator

--- generate.py
+++ generate.py
@@ -1,42 +1,78 @@
 from random import *
 
-def printArr(arr, n) :
-    for i in range(0, n) :
-        if(i == 0) :
-            print(arr[i], end='')
+class letter():
+    def __init__(self, lowerchar, upperchar, isVowel, isConsonant):
+        self.upperchar = upperchar
+        self.lowerchar = lowerchar
+        self.isVowel = isVowel
+        self.isConsonant = isConsonant
+
+global alphabet
+alphabet = [letter('a', 'A', True, False),
+            letter('b', 'B', False, True),
+            letter('c', 'C', False, True),
+            letter('d', 'D', False, True),
+            letter('e', 'E', True, False),
+            letter('f', 'F', False, True),
+            letter('g', 'G', False, True),
+            letter('h', 'H', False, True),
+            letter('i', 'I', True, False),
+            letter('j', 'J', False, True),
+            letter('k', 'K', False, True),
+            letter('l', 'L', False, True),
+            letter('m', 'M', False, True),
+            letter('n', 'N', False, True),
+            letter('o', 'O', True, False),
+            letter('p', 'P', False, True),
+            letter('q', 'Q', False, True),
+            letter('r', 'R', False, True),
+            letter('s', 'S', False, True),
+            letter('t', 'T', False, True),
+            letter('u', 'U', True, False),
+            letter('v', 'V', False, True),
+            letter('w', 'W', False, True),
+            letter('x', 'X', False, True),
+            letter('y', 'Y', True, True),
+            letter('z', 'Z', False, True)]
+
+def makeName():
+    min = 3
+    max = 10
+    nameLength = randint(min, max)
+
+    name = ""
+
+    prevVowel = False
+    prevConsonant = False
+    prev2Vowel = False
+    prev2Consonant = False
+
+    for i in range(0, nameLength):
+        a = getLetter(prev2Vowel, prev2Consonant)
+        if(i == 0):
+            name = name + a.upperchar
         else:
-            print(", ", test[i], end='')
-    print("\n")
+            name = name + a.lowerchar
 
-def partition(arr, low, high):
-    i = low
-    pivot = arr[high]
-    for j in range(low, high):
-        if(arr[j] <= pivot):
-            arr[j],arr[i] = arr[i],arr[j]
-            i += 1
+        prev2Vowel = a.isVowel and prevVowel
+        prev2Consonant = a.isConsonant and prevConsonant
+        prevVowel = a.isVowel
+        prevConsonant = a.isConsonant
 
-    arr[i],arr[high] = arr[high],arr[i]
-    return i
+    return name
 
-def quickSort(arr, lower, upper) :
-    if(lower < upper) :
-        m = partition(arr, lower, upper)
+def getLetter(needConsonant, needVowel):
+    global alphabet
 
-        quickSort(arr, lower, m - 1)
-        quickSort(arr, m + 1, upper)
+    done = False
+    while(not done):
+        a = alphabet[randint(0, 25)]
+        if((needConsonant and a.isVowel) or (needVowel and a.isConsonant)):
+            done = False
+        else:
+            done = True
 
-n = 20
-test = [0] * n
+    return a
 
-for i in range(0, n) :
-    test[i] = randint(0, 100)
-
-print("Unsorted array:")
-printArr(test, n)
-
-print("Sorted array:")
-quickSort(test, 0, n - 1)
-printArr(test, n)
-
-
+yourName = makeName()
+print(yourName)
